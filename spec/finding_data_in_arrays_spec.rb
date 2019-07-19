e678a5d48b4ad39e472053b735220cce7dceade2
Joel Mendoza <joelmendza173@gmail.com>
2019-07-19 04:34:27 +0000

    Automatically backed up by Learn

--- spec/finding_data_in_arrays_spec.rb
+++ spec/finding_data_in_arrays_spec.rb
@@ -3,7 +3,14 @@ require 'pry'
 
 describe 'Finding data' do
 
-  scale = [2,3,4,5]
+  def find_element_index(array, value_to_find)
+  #array.index(value_to_find)
+  array.length.times { |index|
+    if array[index] == value_to_find
+      p array.index(value_to_find)
+    end
+    }
+end
   scale_2 = [6,5,4,3]
   hill = [1,2,3,4,5,4,3,2,1]
   valley = [5,4,3,2,1,0,1,2,3,4,5]
